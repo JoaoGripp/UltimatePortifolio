0a502d6da0af986ece75cb0f30f05f7f48b56712
Joao Gripp <corguinha@gmail.com>
2023-02-17 13:53:40 +0000

    Initial Commit

---
+++ UltimatePortifolio/UltimatePortifolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  UltimatePortifolioApp.swift
+//  UltimatePortifolio
+//
+//  Created by Joao Gripp on 17/02/23.
+//
+
+import SwiftUI
+
+@main
+struct UltimatePortifolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
