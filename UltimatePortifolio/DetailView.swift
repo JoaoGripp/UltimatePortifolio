40cbbb31adf9f533264a631ea8ef54531a694b4a
Joao Gripp <corguinha@gmail.com>
2023-02-20 16:00:46 +0000

    Initial Commit

---
+++ UltimatePortifolio/DetailView.swift
@@ -0,0 +1,20 @@
+//
+//  DetailView.swift
+//  UltimatePortifolio
+//
+//  Created by Joao Gripp on 20/02/23.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text("Detail")
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailView()
+    }
+}
