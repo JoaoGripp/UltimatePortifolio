1b6c56a532b487c30d8cefbae5cf6ac1c8e62713
Joao Gripp <corguinha@gmail.com>
2023-03-01 15:50:24 +0000

    Showing, deleting and synchronizing issues fixed.

---
+++ UltimatePortifolio/IssueRow.swift
@@ -0,0 +1,51 @@
+//
+//  IssueRow.swift
+//  UltimatePortifolio
+//
+//  Created by Joao Gripp on 01/03/23.
+//
+
+import SwiftUI
+
+struct IssueRow: View {
+    @EnvironmentObject var dataController: DataController
+    @ObservedObject var issue: Issue
+
+    var body: some View {
+        NavigationLink(value: issue) {
+            HStack {
+                Image(systemName: "exclamationmark.circle")
+                    .imageScale(.large)
+                    .opacity(issue.priority == 2 ? 1 : 0)
+                
+                VStack(alignment: .leading) {
+                    Text(issue.issueTitle)
+                        .font(.headline)
+                        .lineLimit(1)
+                    Text("No tags")
+                        .foregroundStyle(.secondary)
+                        .lineLimit(1)
+                }
+                
+                Spacer()
+                
+                VStack(alignment: .trailing) {
+                    Text(issue.issueCreationDate.formatted(date: .numeric, time: .omitted))
+                        .font(.body.smallCaps())
+                    
+                    if issue.completed {
+                        Text("CLOSED")
+                            .font(.body.smallCaps())
+                    }
+                }
+                .foregroundStyle(.secondary)
+            }
+        }
+    }
+}
+
+struct IssueRow_Previews: PreviewProvider {
+    static var previews: some View {
+        IssueRow(issue: .example)
+    }
+}
