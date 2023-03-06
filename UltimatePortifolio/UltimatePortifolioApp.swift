187d341f0dcbad27188554b16ee564c1951e5dcb
Joao Gripp <corguinha@gmail.com>
2023-03-06 09:56:00 +0000

    Instant sync and save

--- UltimatePortifolio/UltimatePortifolioApp.swift
+++ UltimatePortifolio/UltimatePortifolioApp.swift
@@ -10,6 +10,7 @@ import SwiftUI
 @main
 struct UltimatePortifolioApp: App {
     @State var dataController = DataController()
+    @Environment(\.scenePhase) var scenePhase
     
     var body: some Scene {
         WindowGroup {
@@ -22,6 +23,11 @@ struct UltimatePortifolioApp: App {
             }
             .environment(\.managedObjectContext, dataController.container.viewContext)
             .environmentObject(dataController)
+            .onChange(of: scenePhase) { phase in
+                if phase != .active {
+                    dataController.save()
+                }
+            }
         }
     }
 }
